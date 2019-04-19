1b749daad79d3e64eca570bc6789a53e76dcc57f
GooRiOn <foreverframenet@gmail.com>
2019-04-19 09:15:16 +0200

    Migrated metrics config to support IServiceCollection

--- src/Convey.Metrics.AppMetrics/Extensions.cs
+++ src/Convey.Metrics.AppMetrics/Extensions.cs
@@ -1,100 +1,129 @@
 using System;
 using App.Metrics;
 using App.Metrics.AspNetCore;
-using App.Metrics.AspNetCore.Health;
+using App.Metrics.AspNetCore.Endpoints;
+using App.Metrics.AspNetCore.Health.Endpoints;
+using App.Metrics.AspNetCore.Tracking;
 using App.Metrics.Formatters.Prometheus;
+using Convey.Metrics.AppMetrics.Builders;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Convey.Metrics.AppMetrics
 {
     public static class Extensions
     {
         private static bool _initialized;
+        private const string SectionName = "appMetrics";
+        private const string RegistryName = "metrics.appMetrics";
 
-        public static IWebHostBuilder AddAppMetrics(this IWebHostBuilder webHostBuilder)
+        public static IConveyBuilder AddAppMetrics(this IConveyBuilder builder, string sectionName = SectionName)
         {
-            if (_initialized)
+            var options = builder.GetOptions<MetricsOptions>(sectionName);
+            return builder.AddAppMetrics(options);
+        }
+        
+        public static IConveyBuilder AddAppMetrics(this IConveyBuilder builder, 
+            Func<IMetricsOptionsBuilder, IMetricsOptionsBuilder> buildOptions)
+        {
+            var options = buildOptions(new MetricsOptionsBuilder()).Build();
+            return builder.AddAppMetrics(options);
+        }
+
+        public static IConveyBuilder AddAppMetrics(this IConveyBuilder builder, MetricsOptions options)
+        {           
+            if (!builder.TryRegister(RegistryName) || !options.Enabled || _initialized)
             {
-                return webHostBuilder;
+                return builder;
             }
 
-            return webHostBuilder
-                .ConfigureMetricsWithDefaults((context, builder) =>
+            _initialized = true;
+            var metricsBuilder = new MetricsBuilder().Configuration.Configure(cfg =>
+            {
+                var tags = options.Tags;
+                if (tags == null)
                 {
-                    var metricsOptions = context.Configuration.GetOptions<MetricsOptions>("metrics");
-                    if (!metricsOptions.Enabled)
-                    {
-                        return;
-                    }
+                    return;
+                }
 
-                    _initialized = true;
-                    builder.Configuration.Configure(cfg =>
-                        {
-                            var tags = metricsOptions.Tags;
-                            if (tags == null)
-                            {
-                                return;
-                            }
-
-                            tags.TryGetValue("app", out var app);
-                            tags.TryGetValue("env", out var env);
-                            tags.TryGetValue("server", out var server);
-                            cfg.AddAppTag(string.IsNullOrWhiteSpace(app) ? null : app);
-                            cfg.AddEnvTag(string.IsNullOrWhiteSpace(env) ? null : env);
-                            cfg.AddServerTag(string.IsNullOrWhiteSpace(server) ? null : server);
-                            foreach (var tag in tags)
-                            {
-                                if (!cfg.GlobalTags.ContainsKey(tag.Key))
-                                {
-                                    cfg.GlobalTags.Add(tag.Key, tag.Value);
-                                }
-                            }
-                        }
-                    );
-
-                    if (metricsOptions.InfluxEnabled)
-                    {
-                        builder.Report.ToInfluxDb(o =>
-                        {
-                            o.InfluxDb.Database = metricsOptions.Database;
-                            o.InfluxDb.BaseUri = new Uri(metricsOptions.InfluxUrl);
-                            o.InfluxDb.CreateDataBaseIfNotExists = true;
-                            o.FlushInterval = TimeSpan.FromSeconds(metricsOptions.Interval);
-                        });
-                    }
-                })
-                .UseHealth()
-                .UseHealthEndpoints()
-                .UseMetricsWebTracking()
-                .UseMetrics((context, options) =>
+                tags.TryGetValue("app", out var app);
+                tags.TryGetValue("env", out var env);
+                tags.TryGetValue("server", out var server);
+                cfg.AddAppTag(string.IsNullOrWhiteSpace(app) ? null : app);
+                cfg.AddEnvTag(string.IsNullOrWhiteSpace(env) ? null : env);
+                cfg.AddServerTag(string.IsNullOrWhiteSpace(server) ? null : server);
+                foreach (var tag in tags)
                 {
-                    var metricsOptions = context.Configuration.GetOptions<MetricsOptions>("metrics");
-                    if (!metricsOptions.Enabled)
+                    if (!cfg.GlobalTags.ContainsKey(tag.Key))
                     {
-                        return;
+                        cfg.GlobalTags.Add(tag.Key, tag.Value);
                     }
-
-                    if (!metricsOptions.PrometheusEnabled)
-                    {
-                        return;
-                    }
-
-                    options.EndpointOptions = endpointOptions =>
-                    {
-                        switch (metricsOptions.PrometheusFormatter?.ToLowerInvariant() ?? string.Empty)
-                        {
-                            case "protobuf":
-                                endpointOptions.MetricsEndpointOutputFormatter =
-                                    new MetricsPrometheusProtobufOutputFormatter();
-                                break;
-                            default:
-                                endpointOptions.MetricsEndpointOutputFormatter =
-                                    new MetricsPrometheusTextOutputFormatter();
-                                break;
-                        }
-                    };
-
+                }
+            });
+            
+            if (options.InfluxEnabled)
+            {
+                metricsBuilder.Report.ToInfluxDb(o =>
+                {
+                    o.InfluxDb.Database = options.Database;
+                    o.InfluxDb.BaseUri = new Uri(options.InfluxUrl);
+                    o.InfluxDb.CreateDataBaseIfNotExists = true;
+                    o.FlushInterval = TimeSpan.FromSeconds(options.Interval);
                 });
+            }
+            var metrics = metricsBuilder.Build();
+            var metricsWebHostOptions = GetMetricsWebHostOptions(options);
+
+            using (var serviceProvider = builder.Services.BuildServiceProvider())
+            {
+                var configuration = serviceProvider.GetService<IConfiguration>();
+                builder.Services.AddHealth();
+                builder.Services.AddHealthEndpoints(configuration);
+                builder.Services.AddMetricsTrackingMiddleware(configuration);
+                builder.Services.AddMetricsEndpoints(configuration);
+                builder.Services.AddSingleton<IStartupFilter>(new DefaultMetricsEndpointsStartupFilter());
+                builder.Services.AddSingleton<IStartupFilter>(new DefaultHealthEndpointsStartupFilter());
+                builder.Services.AddSingleton<IStartupFilter>(new DefaultMetricsTrackingStartupFilter());
+                builder.Services.AddMetricsReportingHostedService(metricsWebHostOptions.UnobservedTaskExceptionHandler);
+                builder.Services.AddMetricsEndpoints(metricsWebHostOptions.EndpointOptions, configuration);
+                builder.Services.AddMetricsTrackingMiddleware(metricsWebHostOptions.TrackingMiddlewareOptions, configuration);
+                builder.Services.AddMetrics(metrics);
+            }
+
+            return builder;
+        }
+
+        private static MetricsWebHostOptions GetMetricsWebHostOptions(MetricsOptions metricsOptions)
+        {
+            var options = new MetricsWebHostOptions();
+            
+            if (!metricsOptions.Enabled)
+            {
+                return options;
+            }
+
+            if (!metricsOptions.PrometheusEnabled)
+            {
+                return options;
+            }
+
+            options.EndpointOptions = endpointOptions =>
+            {
+                switch (metricsOptions.PrometheusFormatter?.ToLowerInvariant() ?? string.Empty)
+                {
+                    case "protobuf":
+                        endpointOptions.MetricsEndpointOutputFormatter =
+                            new MetricsPrometheusProtobufOutputFormatter();
+                        break;
+                    default:
+                        endpointOptions.MetricsEndpointOutputFormatter =
+                            new MetricsPrometheusTextOutputFormatter();
+                        break;
+                }
+            };
+
+            return options;
         }
     }
 }
